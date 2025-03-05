6d5464e67e9cffb944f36067e8609ffcf45e2909
Lichtkind <lichtkind@cpan.org>
2025-03-05 15:48:54 +0100

    adapt outer data nodes

--- dev/release/init/lib/Data/Store/Consistent/Node/Outer.pm
+++ dev/release/init/lib/Data/Store/Consistent/Node/Outer.pm
@@ -9,7 +9,7 @@ use Data::Store::Consistent::Node::Actions;
 
 sub new {
     # my ($pkg, $def) = @_;
-    my ($pkg, $name, $help, $link_up, $type, $writer, $write_call_back, $read_call_back) = @_;
+    my ($pkg, $name, $description, $link_up, $type, $writer, $write_call_back, $read_call_back) = @_;
     return 'need a data set object as first argument' unless ref $type_set eq 'Data::Store::Consistent::Type::Set';
     return 'need a node name as second argument' unless defined $name and $name;
     return 'need help text as third argument' unless defined $help and $help;
@@ -28,11 +28,17 @@ sub new {
     #~ - $write_trigger: @node_path      # trigger writer when on of these nodes changes
 }
 
+sub get_property {
+    my ($self, $property) = @_;
+    # node exists ?
+}
+
 sub read {
-    my ($self) = @_;
+    my ($self, $silent) = @_;
     $self->{'actions'}->trigger('read');
     $self->{'value'}; # clone?
 }
+
 
 sub write {
     my ($self, $data) = @_;
@@ -47,7 +53,7 @@ sub write {
 }
 
 sub reset {
-    my ($self) = @_;
+    my ($self, $silent) = @_;
     # also stop if readonly
     return $error if $error; # + params
     $error = $self->{'eq_checker'}->($data, $self->{'value'}, $self->{'name'});

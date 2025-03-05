c653e47ce50bdea02b1dd97fcede0adc5bdc8ea0
Lichtkind <lichtkind@cpan.org>
2025-03-05 23:44:51 +0100

    copy over data clone algorithm

--- dev/release/init/lib/Data/Store/Consistent/Node/Root.pm
+++ dev/release/init/lib/Data/Store/Consistent/Node/Root.pm
@@ -106,3 +106,32 @@ sub fill {
 
 
 1;
+
+__END__
+
+sub clone      { _clone($_[0],{})}# data --> data
+sub _clone {
+    my ($data, $dict) = @_;
+    return unless defined $data;
+
+    my ($class, $ref) = (blessed($data), ref $data);
+    return $data if $copied_reftype{$ref};   # ref to readonly data
+    return $dict->{$data} if $dict->{$data}; # reuse ref to already copied data
+
+    my $ret = $ref eq 'HASH' ? {} : $ref eq 'ARRAY' ? [] : '';
+    $dict->{$data} = $ret if $ref eq 'HASH' or $ref eq 'ARRAY';
+    if    ($ref eq 'HASH')   { $ret->{$_} = _clone($data->{$_}, $dict) for keys %$data }
+    elsif ($ref eq 'ARRAY')  { push @$ret, _clone($_, $dict) for @$data  }
+    elsif ($ref eq 'REF')    { $ret = \_clone($$data, $dict);  $dict->{$data} = $ret;}
+    elsif ($ref eq 'SCALAR'
+        or $ref eq 'VSTRING'){ my $val = $$data; $ret = \$val }
+    else                     { } # ?
+
+    $class ? bless($ret, $class) : $ret;
+}
+
+sub clone_list { #               [data] --> [data]
+    my @ret;
+    push @ret, clone_item($_) for @_;
+    @ret;
+}

64b40500f54a14d9994108bd804d0701b8a455ac
Lichtkind <lichtkind@cpan.org>
2025-03-08 20:47:53 +0100

    tidy up default typ defs

--- dev/release/init/lib/Data/Store/Consistent/Type/Default.pm
+++ dev/release/init/lib/Data/Store/Consistent/Type/Default.pm
@@ -12,9 +12,9 @@ our @basic = (
  {name=> 'not_ref',   help=> 'not a reference',         code=> 'not ref $value',             parent=> 'defined',               },
  {name=> 'bool',      help=> '0 or 1',                  code=> '$value eq 0 or $value eq 1', parent=> 'not_ref', default=> 0,  },
  {name=> 'num',       help=> 'any type of number',      code=> 'looks_like_number($value)',  parent=> 'not_ref', default=> 0,  equality=> '$a == $b', },
- {name=> 'int',       help=> 'number without decimals', code=> 'int($value) == $value',      parent=> 'num',     default=> 0,  equality=> '$a == $b', },
+ {name=> 'int',       help=> 'number without decimals', code=> 'int($value) == $value',      parent=> 'num',     default=> 0,  },
  {name=> 'str',       help=> 'string of characters',                                         parent=> 'not_ref',               },
- {name=> 'char',      help=> 'one letter',              code=> 'length($value) == 1',        parent=> 'str',     default=> 'a' },
+ {name=> 'char',      help=> 'one character',           code=> 'length($value) == 1',        parent=> 'str',     default=> 'a' },
  {name=> 'ne_str',    help=> 'none empty string',       code=> '$value',                     parent=> 'not_ref', default=> ' ' },
  {name=> 'lc_str',    help=> 'lower case string',       code=> 'lc $value eq $value',        parent=> 'ne_str',  default=> 'a' },
  {name=> 'uc_str',    help=> 'upper case string',       code=> 'uc $value eq $value',        parent=> 'ne_str',  default=> 'A' },
@@ -24,45 +24,38 @@ our @basic = (
 );
 
 our @parametric = (
- {name=> 'min',       help=> 'greater or equal $param', code=> '$value >= $param',           parent => 'num'  }, # or children of num
- {name=> 'inf',       help=> 'greater then $param',     code=> '$value >  $param',           parent => 'num'  },
- {name=> 'max',       help=> 'less or equal $param',    code=> '$value <= $param',           parent => 'num'  },
- {name=> 'sup',       help=> 'less then $param',        code=> '$value <  $param',           parent => 'num'  },
- {name=> 'enum',      help=> 'one of: @$param',         code=> '$value eq 0 or $value eq 1', parent => 'str'  },
- {name=> 'ref',       help=> 'ref type $param',         code=> 'ref $value eq $param'                                 },
+ {name=> 'min',       help=> 'greater or equal $param', code=> '$value >= $param',           parent => 'num',   param_name=> 'minimum of $arg', },
+ {name=> 'inf',       help=> 'greater then $param',     code=> '$value >  $param',           parent => 'num',   param_name=> 'infimum of $arg', },
+ {name=> 'max',       help=> 'less or equal $param',    code=> '$value <= $param',           parent => 'num',   param_name=> 'maximum of $arg'  },
+ {name=> 'sup',       help=> 'less then $param',        code=> '$value <  $param',           parent => 'num',   param_name=> 'supremum of $arg' },
+ {name=> 'ref',       help=> 'ref type $param',         code=> 'ref $value eq $param'                           param_name=> '$arg reference' },
 );
 
 our @argument = (
- {name=> 'pos',       help=> 'positive number',         parent=> 'min',      arg=> 0 },
- {name=> 'spos',      help=> 'strictly positive number',parent=> 'inf',      arg=> 0 },
- {name=> 'array',     help=> 'ARRAY reference',         parent=> 'ref',      arg=> 'ARRAY' },
- {name=> 'hash',      help=> 'HASH reference',          parent=> 'ref',      arg=> 'HASH' },
- {name=> 'code',      help=> 'CODE reference',          parent=> 'ref',      arg=> 'CODE' },
- {name=> 'char',      help=> 'an string of length 1',    code=> 'keys(%$value)',  input=> 'hash' ,     output=> 'int' },
+ {name=> 'pos',       help=> 'positive number',         parent=> 'min',      value=> 0 },
+ {name=> 'spos',      help=> 'strictly positive number',parent=> 'inf',      value=> 0 },
+ {name=> 'array',     help=> 'ARRAY reference',         parent=> 'ref',      value=> 'ARRAY' },
+ {name=> 'hash',      help=> 'HASH reference',          parent=> 'ref',      value=> 'HASH' },
+ {name=> 'code',      help=> 'CODE reference',          parent=> 'ref',      value=> 'CODE' },
 );
 
 our @property = (
- {name=> 'len',       help=> '',                                 code=> '$value',         input=> 'num'  ,     output=> '' },
- {name=> 'len',       help=> 'an string with length of $param',  code=> 'length($value)', input=> 'str'  ,     output=> 'int' },
- {name=> 'len',       help=> 'an Array of length $param',        code=> '@$value',        input=> 'array',     output=> 'int' },
- {name=> 'len',       help=> 'an Hash of length $param',         code=> 'keys(%$value)',  input=> 'hash' ,     output=> 'int' },
+ {name=> 'length',    help=> '',                                 code=> '$value',         parent=> 'num'  ,         type_name=> '' },
+ {name=> 'length',    help=> 'an string with length of $param',  code=> 'length($value)', parent=> 'str'  ,         type_name=> 'int' },
+ {name=> 'length',    help=> 'number of ARRAY elements',         code=> '@$value',        parent=> ['ref','array'], type_name=> 'int' },
+ {name=> 'length',    help=> 'number of HASH keys',              code=> 'keys(%$value)',  parent=> ['ref','hash'] , type_name=> 'int' },
 );
 
 our @combinator = (
- {name=> 'LIST',      help=> 'list',       parent=> ['str', 'num', 'int'],
-                      code=> ['for my $index (0 .. $#param) { my $param = $param[$index];',,'}'], },
- {name=> 'IN_SET',    help=> '',           parent=> 'str' , },
- {name=> 'ARRAY',     help=> '',           parent=> 'array',               property=> ['index', 'element'],
-                      code=> ['for my $index (0 .. $#value) { my $value = $value[$index];',,'}'],
- },
- {name=> 'HASH',      help=> '',           parent=> 'hash',                property=> ['key', 'value'],
-                      code=> ['for my $key (keys %value) { my $value = $value{$key};',,'}'], },
- {name=> 'OR',        help=> 'alternative',  parent=> '',
-                      code=> '1', },
+ {name=> 'OR',        help=> '',           parent=> 'str' ,     },
+ {name=> 'IN_SET',    help=> 'value is part of SET',    parent=> 'ne_str', type_name=> 'bool',
+                      code=> ['for my $index (0 .. $#param) { my $param = $value[$index];','}'], },
+ {name=> 'ARRAY',     help=> 'ARRAY of typed elements', parent=> ['ref','array'],        property=> {index => 1, element => 1},
+                      code=> ['for my $index (0 .. $#value) { my $value = $value[$index];','}'],  },
+ {name=> 'HASH',      help=> 'HASH of typed elements',  parent=> ['ref','hash'],         property=> {key => 1, value => 1},
+                      code=> ['for my $key (keys %value) { my $value = $value{$key};'."\n","}\n"], },
 );
-
-
 
 1;
 
-# durchgangstypen op liek not ?
+# op types  like: not, lc, uc ?
